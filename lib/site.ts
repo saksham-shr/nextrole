dec473d92205e3a6037bcacb58742727cd75c387
saksham-shr <sakshamsharma614@gmail.com>
2026-04-27 15:06:48 +0530

    Sitemap Updation

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,3 @@
 export function getSiteUrl() {
-  return (process.env.NEXT_PUBLIC_SITE_URL ?? "https://nextrole.app").replace(/\/$/, "");
+  return (process.env.NEXT_PUBLIC_SITE_URL ?? "https://nextrole.live").replace(/\/$/, "");
 }
